bdceed86a01a7f3e87cf8ba410e3b3abc1b173f6
Carol <yuyingguo@139.com>
2015-04-08 21:20:16 +0800

    mark

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,7 +1,9 @@
 define(function(require,exports,module) {
     var reload = require("../javascripts/reload");
+    var menu = require("../javascripts/menu");
 
     reload.reload();
+    menu.menu();
 
     $(window).on('hashchange', reload.reload);
 
---
+++ public/javascripts/menu.js
@@ -0,0 +1,111 @@
+/**
+ * Created by carol on 2015/4/8.
+ */
+define(function(require, exports, module) {
+    var pos1 = {
+        left: '226px',
+        top: '32px'
+        },
+        pos2 = {
+            left: '125px',
+            top: '152px'
+        },
+        pos3 = {
+            left: '327px',
+            top: '152px'
+        };
+    var topAnimation = null;
+    var bottomAnimation = null;
+
+    var htmlLine = '<div id="line-chart">' +
+                        '<div class="line-chart-list"><\/div>' +
+                    '<\/div>';
+    var htmlPie = '<div id="pie-chart">' +
+                        '<div class="pie-chart-list"><\/div>' +
+                    '<\/div>';
+
+    var htmlColumn = '<div id="column-chart">' +
+                        '<div class="column-chart-list"><\/div>' +
+                    '<\/div>';
+
+    exports.menu = function() {
+        $('#top-menu-button').click(function() {
+            if($('#top-menu-list').css('visibility') != 'hidden') {
+                clearInterval(topAnimation);
+                $('#top-menu-list').css('visibility', 'hidden');
+            } else {
+                topAnimation = setInterval(function() {
+                    return (function() {
+                        $('#top-menu-list .line').animate(pos2, 5000);
+                        $('#top-menu-list .line').animate(pos3, 5000);
+                        $('#top-menu-list .line').animate(pos1, 5000);
+                        $('#top-menu-list .pie').animate(pos3, 5000);
+                        $('#top-menu-list .pie').animate(pos1, 5000);
+                        $('#top-menu-list .pie').animate(pos2, 5000);
+                        $('#top-menu-list .column').animate(pos1, 5000);
+                        $('#top-menu-list .column').animate(pos2, 5000);
+                        $('#top-menu-list .column').animate(pos3, 5000);
+                    })
+                }(),1000);
+                $('#top-menu-list').css('visibility', 'visible');
+            }
+        });
+
+        $('#bottom-menu-button').click(function() {
+            if($('#bottom-menu-list').css('visibility') != 'hidden') {
+                clearInterval(bottomAnimation);
+                $('#bottom-menu-list').css('visibility', 'hidden');
+            } else {
+                bottomAnimation = setInterval(function() {
+                        $('#bottom-menu-list .line').animate(pos2, 5000);
+                        $('#bottom-menu-list .line').animate(pos3, 5000);
+                        $('#bottom-menu-list .line').animate(pos1, 5000);
+                        $('#bottom-menu-list .pie').animate(pos3, 5000);
+                        $('#bottom-menu-list .pie').animate(pos1, 5000);
+                        $('#bottom-menu-list .pie').animate(pos2, 5000);
+                        $('#bottom-menu-list .column').animate(pos1, 5000);
+                        $('#bottom-menu-list .column').animate(pos2, 5000);
+                        $('#bottom-menu-list .column').animate(pos3, 5000);
+                }(),1000);
+              $('#bottom-menu-list').css('visibility', 'visible');
+            }
+        });
+
+        $('#top-menu-list .line').click(function() {
+            clearInterval(topAnimation);
+            $('#top-menu-list').css('visibility', 'hidden');
+            $('#left-charts-top')[0].innerHTML = htmlLine;
+        });
+
+        $('#top-menu-list .pie').click(function() {
+            clearInterval(topAnimation);
+            $('#top-menu-list').css('visibility', 'hidden');
+            $('#left-charts-top')[0].innerHTML = htmlPie;
+        });
+
+        $('#top-menu-list .column').click(function() {
+            clearInterval(topAnimation);
+            $('#top-menu-list').css('visibility', 'hidden');
+            $('#left-charts-top')[0].innerHTML = htmlColumn;
+        });
+
+        $('#bottom-menu-list .line').click(function() {
+            clearInterval(bottomAnimation);
+            $('#bottom-menu-list').css('visibility', 'hidden');
+            $('#left-charts-bottom')[0].innerHTML = htmlLine;
+        });
+
+        $('#bottom-menu-list .pie').click(function() {
+            clearInterval(bottomAnimation);
+            $('#bottom-menu-list').css('visibility', 'hidden');
+            $('#left-charts-bottom')[0].innerHTML = htmlPie;
+        });
+
+        $('#bottom-menu-list .column').click(function() {
+            clearInterval(bottomAnimation);
+            $('#bottom-menu-list').css('visibility', 'hidden');
+            $('#left-charts-bottom')[0].innerHTML = htmlColumn;
+        });
+
+    }
+})

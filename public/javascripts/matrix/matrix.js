b2b3b2394c5cd4da7a8b9b4cfa6719e8290e10ec
Carol <yuyingguo@139.com>
2015-04-13 12:03:17 +0800

    ok

--- public/javascripts/matrix/matrix.js
+++ public/javascripts/matrix/matrix.js
@@ -40,6 +40,45 @@ define(function(require, exports, module) {
     var _gMainId = 'matrix-main-area';
     var _gTooltipId = 'matrix-tooltip';
 
+    var svgClientWidth = null;
+    var svgClientHeight = null;
+
+    var xAxisPos = {
+        x: 0,
+        y: 0,
+        step: 0
+    };
+    var yAxisPos = {
+        x: 0,
+        y: 0,
+        step: 0
+    };
+    var rectPos =  {
+        x: 0,
+        y: 0,
+        stepX:0,
+        stepY: 0,
+        width: 0,
+        height: 0
+    };
+
+    function layoutInit(length) {  //根据要显示的关键词数量和当前svg的长宽进行布局的总体分布
+        //横坐标的分布
+        xAxisPos.step = (svgClientWidth - 80) / (length + 1);
+        xAxisPos.x = 40 + xAxisPos.step;
+        xAxisPos.y = 40;
+        //纵坐标的分布
+        yAxisPos.step = (svgClientHeight - 60) / length;
+        yAxisPos.x = 40;
+        yAxisPos.y = 40 + yAxisPos.step;
+        //矩阵分布
+        rectPos.stepX = (svgClientWidth - 80) / (length + 1);
+        rectPos.stepY = (svgClientHeight - 60) / length;
+        rectPos.x = xAxisPos.x;
+        rectPos.y = 40 + yAxisPos.step / 2;
+        rectPos.width = rectPos.stepX - 1;
+        rectPos.height = rectPos.stepY - 1;
+    }
 
     function randomColors(numberOfColors) {
         var colors = [];
@@ -58,12 +97,12 @@ define(function(require, exports, module) {
     }
 
     function createTextNode(pos, content) {   //创建文本节点
-        var text = makeSVG("text",{x: pos.x, y: pos.y, style: 'font-size: 14px'});
+        var text = makeSVG("text",{x: pos.x, y: pos.y, style: 'font-size: 10px'});
         text.appendChild(document.createTextNode(content));
         return text;
     }
 
-    function createALink(href, size) {  //创造一个链接节点,注意在这里给设置节点和属性时必须指定命名空间
+    function createALink(href) {  //创造一个链接节点,注意在这里给设置节点和属性时必须指定命名空间
         var a = document.createElementNS('http://www.w3.org/2000/svg','a');
         a.setAttributeNS('http://www.w3.org/1999/xlink',
             'xlink:href',
@@ -88,34 +127,67 @@ define(function(require, exports, module) {
         a.appendChild(text);*/
     }
 
-    function createRect(pos, size, params) { // style: 'fill:lightgrey' 创建一个矩形节点
-        var rect= makeSVG('rect', {x: pos.x, y: pos.y, width: size.width, height: size.height, id: params.id, style: params.style});
+    function createRect(pos, params) { // style: 'fill:lightgrey' 创建一个矩形节点
+        var rect= makeSVG('rect', {x: pos.x, y: pos.y, width: pos.width, height: pos.height, id: params.id, style: params.style});
         return rect;
     }
 
     function createxAxis(xData) {
-        var pos = {
-            x: 20,
-            y: 40
-        };
-        for(var i = 0; i < xData.length; i++) {
+        var len = null;
+        if(xData.length > 10) {
+            len = 10;
+        } else {
+            len = xData.length;
+        }
+        for(var i = 0; i < len; i++) {
             //var href = shareParams.shareParams._keyword_url + xData[i].trim();
             //var content = xData[i].trim();
             var href = "http://www.baidu.com";
-            var aNode = createALink(href,'{width: 100px, height: 100px}');
-            var textNode = createTextNode(pos, '计算机');
+            var aNode = createALink(href);
+            var textNode = createTextNode(xAxisPos, '计算机');
             $(aNode).append(textNode);
             document.getElementById(_gRowId).appendChild(aNode);
-            pos.y = pos.y + 20;
+            xAxisPos.x = xAxisPos.x + xAxisPos.step;
         }
     }
 
     function createyAxis(yData) {
-
+        var len = null;
+        if(yData.length > 10) {
+            len = 10;
+        } else {
+            len = yData.length;
+        }
+        for(var i = 0; i < len; i++) {
+            //var href = shareParams.shareParams._keyword_url + xData[i].trim();
+            //var content = xData[i].trim();
+            var href = "http://www.baidu.com";
+            var aNode = createALink(href);
+            var textNode = createTextNode(yAxisPos, '计算机');
+            $(aNode).append(textNode);
+            document.getElementById(_gColumnId).appendChild(aNode);
+            yAxisPos.y = yAxisPos.y + yAxisPos.step;
+        }
     }
 
     function createMain(data) {
-
+        var len = null;
+        if(data.keywordsArray.length > 10) {
+            len = 10;
+        } else {
+            len = data.keywordsArray.length;
+        }
+        for(var i = 0; i < len; i++) {
+            //var href = shareParams.shareParams._keyword_url + xData[i].trim();
+            //var count = data.dataJSON.patentsArray[i].count.trim();
+            for(var j = i; j < len; j++) {
+                var fillStyle = 'fill:' + defaultOptions.colors[j];
+                var rectNode = createRect(rectPos, {style: fillStyle});
+                document.getElementById(_gMainId).appendChild(rectNode);
+                rectPos.x = rectPos.x + rectPos.stepX;
+            }
+            rectPos.y = rectPos.y + rectPos.stepY;
+        }
     }
 
     function createTooltip(data) {
@@ -123,12 +195,12 @@ define(function(require, exports, module) {
     }
 
     function drawMatrix(data) {
+        var len = data.dataJSON.keywordsArray.length > 10 ? 10 : data.dataJSON.keywordsArray.length;
+        layoutInit(len);
+
         xAxisHTML = createxAxis(data.dataJSON.keywordsArray);
-
-        var allHTML = xAxisHTML + yAxisHTML + mainHTML + tooltipHTML;
-
-        //$(_gRowId)[0].innerHTML = xAxisHTML;
-       // console.log($(_gRowId)[0].innerHTML);
+        yAxisHTML = createyAxis(data.dataJSON.keywordsArray);
+        mainHTML = createMain(data.dataJSON);
         //添加交互事件
         addEvent();
     }
@@ -137,8 +209,8 @@ define(function(require, exports, module) {
         //添加交互事件
     }
     exports.matrix = function(options) {
-        defaultOptions.color = randomColors(options.dataJSON.keywordsArray.length);
-
+        defaultOptions.colors = randomColors(options.dataJSON.keywordsArray.length);
+        console.log(defaultOptions.colors.length)
         options = $.extend(defaultOptions, options);
         var id = '#' + options.id;
         container = $(id);    //容器
@@ -155,6 +227,9 @@ define(function(require, exports, module) {
         svgElement.appendChild(gTooltipElement);
 
         document.getElementById(options.id).appendChild(svgElement);
+
+        svgClientWidth = svgElement.clientWidth;
+        svgClientHeight = svgElement.clientHeight;
 
         drawMatrix(options);
         //console.log('default: ' + options.dataJSON.keywordsArray);
